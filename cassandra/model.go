0d8fd0570ac654e05d7db788a545e68c87b19c24
Duc Nguyen <minhduc140583@gmail.com>
2024-06-30 20:18:03 +0700

    Remove firestore, mongo, elasticsearch, casandra

--- cassandra/model.go
+++
@@ -1,6 +0,0 @@
-package cassandra
-
-type Statement struct {
-	Query  string        `mapstructure:"query" json:"query,omitempty" gorm:"column:query" bson:"query,omitempty" dynamodbav:"query,omitempty" firestore:"query,omitempty"`
-	Params []interface{} `mapstructure:"params" json:"params,omitempty" gorm:"column:params" bson:"params,omitempty" dynamodbav:"params,omitempty" firestore:"params,omitempty"`
-}
--- mongo/geo.go
+++
@@ -1,6 +0,0 @@
-package mongo
-
-type JSON struct {
-	Type        string    `json:"type,omitempty" bson:"type,omitempty"`
-	Coordinates []float64 `json:"coordinates,omitempty" bson:"coordinates,omitempty"`
-}
