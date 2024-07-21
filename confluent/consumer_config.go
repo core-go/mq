6ee81d18f788f2d02da1d7e8a6f8c2177124c2bf
Duc Nguyen <minhduc140583@gmail.com>
2024-07-21 21:22:15 +0700

    Refactor confluentinc

--- confluent/consumer_config.go
+++ confluent/consumer_config.go
@@ -1,10 +1,10 @@
 package kafka
 
 type ConsumerConfig struct {
-	Brokers        []string     `mapstructure:"brokers" json:"brokers,omitempty" gorm:"column:brokers" bson:"brokers,omitempty" dynamodbav:"brokers,omitempty" firestore:"brokers,omitempty"`
-	GroupID        string       `mapstructure:"group_id" json:"groupID,omitempty" gorm:"column:groupid" bson:"groupID,omitempty" dynamodbav:"groupID,omitempty" firestore:"groupID,omitempty"`
-	Topic          string       `mapstructure:"Topic" json:"Topic,omitempty" gorm:"column:Topic" bson:"Topic,omitempty" dynamodbav:"Topic,omitempty" firestore:"Topic,omitempty"`
-	Client         ClientConfig `mapstructure:"client" json:"client,omitempty" gorm:"column:client" bson:"client,omitempty" dynamodbav:"client,omitempty" firestore:"client,omitempty"`
-	InitialOffsets *int64       `mapstructure:"initial_offsets" json:"initialOffsets,omitempty" gorm:"column:initialoffsets" bson:"initialOffsets,omitempty" dynamodbav:"initialOffsets,omitempty" firestore:"initialOffsets,omitempty"`
-	AckOnConsume   bool         `mapstructure:"ack" json:"ack,omitempty" gorm:"column:ack" bson:"ack,omitempty" dynamodbav:"ack,omitempty" firestore:"ack,omitempty"`
+	Brokers        []string     `yaml:"brokers" mapstructure:"brokers" json:"brokers,omitempty" gorm:"column:brokers" bson:"brokers,omitempty" dynamodbav:"brokers,omitempty" firestore:"brokers,omitempty"`
+	GroupID        string       `yaml:"group_id" mapstructure:"group_id" json:"groupID,omitempty" gorm:"column:groupid" bson:"groupID,omitempty" dynamodbav:"groupID,omitempty" firestore:"groupID,omitempty"`
+	Topic          string       `yaml:"topic" mapstructure:"topic" json:"topic,omitempty" gorm:"column:topic" bson:"topic,omitempty" dynamodbav:"topic,omitempty" firestore:"topic,omitempty"`
+	Client         ClientConfig `yaml:"client" mapstructure:"client" json:"client,omitempty" gorm:"column:client" bson:"client,omitempty" dynamodbav:"client,omitempty" firestore:"client,omitempty"`
+	InitialOffsets *int64       `yaml:"initial_offsets" mapstructure:"initial_offsets" json:"initialOffsets,omitempty" gorm:"column:initialoffsets" bson:"initialOffsets,omitempty" dynamodbav:"initialOffsets,omitempty" firestore:"initialOffsets,omitempty"`
+	AckOnConsume   bool         `yaml:"ack" mapstructure:"ack" json:"ack,omitempty" gorm:"column:ack" bson:"ack,omitempty" dynamodbav:"ack,omitempty" firestore:"ack,omitempty"`
 }
--- confluent/producer_config.go
+++ confluent/producer_config.go
@@ -1,18 +1,18 @@
 package kafka
 
 type ProducerConfig struct {
-	Brokers         []string       `mapstructure:"brokers" json:"brokers,omitempty" gorm:"column:brokers" bson:"brokers,omitempty" dynamodbav:"brokers,omitempty" firestore:"brokers,omitempty"`
-	Topic           string         `mapstructure:"topic" json:"topic,omitempty" gorm:"column:topic" bson:"topic,omitempty" dynamodbav:"topic,omitempty" firestore:"topic,omitempty"`
-	Timeout         int            `mapstructure:"timeout" json:"timeout,omitempty" gorm:"column:timeout" bson:"timeout,omitempty" dynamodbav:"timeout,omitempty" firestore:"timeout,omitempty"`
-	Client          ClientConfig   `mapstructure:"client" json:"client,omitempty" gorm:"column:client" bson:"client,omitempty" dynamodbav:"client,omitempty" firestore:"client,omitempty"`
-	MaxOpenRequests *int           `mapstructure:"max_open_requests" json:"maxOpenRequests,omitempty" gorm:"column:maxopenrequests" bson:"maxOpenRequests,omitempty" dynamodbav:"maxOpenRequests,omitempty" firestore:"maxOpenRequests,omitempty"`
-	RequiredAcks    *int16         `mapstructure:"required_acks" json:"requiredAcks,omitempty" gorm:"column:requiredacks" bson:"requiredAcks,omitempty" dynamodbav:"requiredAcks,omitempty" firestore:"requiredAcks,omitempty"`
-	Idempotent      *bool          `mapstructure:"idempotent" json:"idempotent,omitempty" gorm:"column:idempotent" bson:"idempotent,omitempty" dynamodbav:"idempotent,omitempty" firestore:"idempotent,omitempty"`
-	ReturnSuccesses *bool          `mapstructure:"return_successes" json:"returnSuccesses,omitempty" gorm:"column:returnsuccesses" bson:"returnSuccesses,omitempty" dynamodbav:"returnSuccesses,omitempty" firestore:"returnSuccesses,omitempty"`
-	Retry           *ProducerRetry `mapstructure:"retry" json:"retry,omitempty" gorm:"column:retry" bson:"retry,omitempty" dynamodbav:"retry,omitempty" firestore:"retry,omitempty"`
+	Brokers         []string       `yaml:"brokers" mapstructure:"brokers" json:"brokers,omitempty" gorm:"column:brokers" bson:"brokers,omitempty" dynamodbav:"brokers,omitempty" firestore:"brokers,omitempty"`
+	Topic           string         `yaml:"topic" mapstructure:"topic" json:"topic,omitempty" gorm:"column:topic" bson:"topic,omitempty" dynamodbav:"topic,omitempty" firestore:"topic,omitempty"`
+	Timeout         int            `yaml:"timeout" mapstructure:"timeout" json:"timeout,omitempty" gorm:"column:timeout" bson:"timeout,omitempty" dynamodbav:"timeout,omitempty" firestore:"timeout,omitempty"`
+	Client          ClientConfig   `yaml:"client" mapstructure:"client" json:"client,omitempty" gorm:"column:client" bson:"client,omitempty" dynamodbav:"client,omitempty" firestore:"client,omitempty"`
+	MaxOpenRequests *int           `yaml:"max_open_requests" mapstructure:"max_open_requests" json:"maxOpenRequests,omitempty" gorm:"column:maxopenrequests" bson:"maxOpenRequests,omitempty" dynamodbav:"maxOpenRequests,omitempty" firestore:"maxOpenRequests,omitempty"`
+	RequiredAcks    *int16         `yaml:"required_acks" mapstructure:"required_acks" json:"requiredAcks,omitempty" gorm:"column:requiredacks" bson:"requiredAcks,omitempty" dynamodbav:"requiredAcks,omitempty" firestore:"requiredAcks,omitempty"`
+	Idempotent      *bool          `yaml:"idempotent" mapstructure:"idempotent" json:"idempotent,omitempty" gorm:"column:idempotent" bson:"idempotent,omitempty" dynamodbav:"idempotent,omitempty" firestore:"idempotent,omitempty"`
+	ReturnSuccesses *bool          `yaml:"return_successes" mapstructure:"return_successes" json:"returnSuccesses,omitempty" gorm:"column:returnsuccesses" bson:"returnSuccesses,omitempty" dynamodbav:"returnSuccesses,omitempty" firestore:"returnSuccesses,omitempty"`
+	Retry           *ProducerRetry `yaml:"retry" mapstructure:"retry" json:"retry,omitempty" gorm:"column:retry" bson:"retry,omitempty" dynamodbav:"retry,omitempty" firestore:"retry,omitempty"`
 }
 
 type ProducerRetry struct {
-	Max     *int  `mapstructure:"max" json:"max,omitempty" gorm:"column:max" bson:"max,omitempty" dynamodbav:"max,omitempty" firestore:"max,omitempty"`
-	Backoff int64 `mapstructure:"backoff" json:"backoff,omitempty" gorm:"column:backoff" bson:"backoff,omitempty" dynamodbav:"backoff,omitempty" firestore:"backoff,omitempty"`
+	Max     *int  `yaml:"max" mapstructure:"max" json:"max,omitempty" gorm:"column:max" bson:"max,omitempty" dynamodbav:"max,omitempty" firestore:"max,omitempty"`
+	Backoff int64 `yaml:"backoff" mapstructure:"backoff" json:"backoff,omitempty" gorm:"column:backoff" bson:"backoff,omitempty" dynamodbav:"backoff,omitempty" firestore:"backoff,omitempty"`
 }
